7ef056348d2b730dda887285e2623f4b140e6081
ahmedgoweda <ahmedgoweda94@gmail.com>
2024-07-16 20:51:24 +0300

    add build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './components/navbar/navbar';
+
 import Home from './components/Home/Home';
 
 
@@ -9,8 +9,7 @@ import Home from './components/Home/Home';
  function App() {
   return (
     <> 
-    
-      <Navbar></Navbar>
+
       <Home></Home>
 </>
   );
